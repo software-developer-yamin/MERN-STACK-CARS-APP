8a4d993c30a4daa0f85bd0a127fdc50d404d9000
DeveloperYamin <developer.yamin@gmail.com>
2022-02-18 17:45:46 +0600

    create navitems

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 import Logo from "../logo";
+import NavbarItems from "./navbarItems";
 
 const NavbarContainer = styled.div`
   min-height: 68px;
   ${tw`
      w-full
-     max-w-2xl
+     max-w-screen-2xl
      flex
      flex-row
      items-center
@@ -22,6 +23,7 @@ function Navbar() {
       <LogoContainer>
         <Logo />
       </LogoContainer>
+      <NavbarItems/>
     </NavbarContainer>
   );
 }
---
+++ src/app/components/navbar/navbarItems.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const ListContainer = styled.ul`
+  ${tw`
+          flex
+          list-none
+     `};
+`;
+const ListItem = styled.li`
+  ${tw`
+          text-xs
+          md:text-base
+          text-black
+          font-medium
+          mr-1
+          md:mr-5
+          cursor-pointer
+          transition
+          duration-300
+          ease-in-out
+          hover:text-gray-700
+     `};
+`;
+
+function NavbarItems() {
+  return (
+    <ListContainer>
+      <ListItem>
+        <a href="#">Home</a>
+      </ListItem>
+      <ListItem>
+        <a href="#">Cars</a>
+      </ListItem>
+      <ListItem>
+        <a href="#">Services</a>
+      </ListItem>
+      <ListItem>
+        <a href="#">Contact Us</a>
+      </ListItem>
+    </ListContainer>
+  );
+}
+
+export default NavbarItems;
