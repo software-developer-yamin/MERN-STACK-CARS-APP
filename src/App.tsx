86cfc7769ef9ed4c20cf4511cb908ca40a8f292d
DeveloperYamin <developer.yamin@gmail.com>
2022-02-18 16:21:44 +0600

    Setting Up Frontend Project and Installing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import HomePage from "./app/components/containers/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/app/components/containers/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const PageContainer = styled.div`
+  ${tw`
+        flex  
+     `};
+`;
+
+const HomePage = () => {
+  return <PageContainer>HomePage</PageContainer>;
+};
+
+export default HomePage;
