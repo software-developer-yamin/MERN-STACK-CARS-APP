37a4a259139c172b85ba869f59013389356af955
DeveloperYamin <developer.yamin@gmail.com>
2022-02-20 01:12:54 +0600

    Using custom responsive carousel component for rendering car cards

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -1,21 +1,29 @@
+import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { ICar } from "../../../typings/car";
 import { Car } from "../../components/car";
+import { SCREENS } from "../../components/responsive";
 
 const TopCarsContainer = styled.div`
   ${tw`
-          w-full
-          max-w-screen-2xl
-          flex
-          flex-col
-          justify-center
-          items-center
-          px-4
-          md:px-0
-          mb-10
-     `}
+    max-w-screen-lg
+    w-full
+    flex
+    flex-col
+    items-center
+    justify-center
+    pr-4
+    pl-4
+    md:pl-0
+    md:pr-0
+    mb-10
+  `};
 `;
+
 const Title = styled.h2`
   ${tw`
     text-3xl
@@ -24,48 +32,122 @@ const Title = styled.h2`
     font-extrabold
   `};
 `;
+
 const CarsContainer = styled.div`
   ${tw`
-          w-full
-          flex
-          flex-wrap
-          justify-center
-          mt-7
-          md:mt-10
-     `}
+    w-full
+    flex
+    flex-wrap
+    justify-center
+    mt-7
+    md:mt-10
+  `};
 `;
 
-function TopCars() {
+const EmptyCars = styled.div`
+  ${tw`
+    w-full
+    flex
+    justify-center
+    items-center
+    text-sm
+    text-gray-500
+  `};
+`;
+
+const LoadingContainer = styled.div`
+  ${tw`
+    w-full
+    mt-9
+    flex
+    justify-center
+    items-center
+    text-base
+    text-black
+  `};
+`;
+
+export default function TopCars() {
+  const [current, setCurrent] = useState(0);
+
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
   const testCar: ICar = {
-    name: "name",
-    mileage: "mileage",
+    name: "Audi S3 Car",
+    mileage: "10k",
     thumbnailSrc:
-      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
-    dailyPrice: 1000,
-    monthlyPrice: 5000,
-    gearType: "gearType",
-    gas: "gas",
+      "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+    dailyPrice: 70,
+    monthlyPrice: 1600,
+    gearType: "Auto",
+    gas: "Petrol",
   };
+
   const testCar2: ICar = {
-    name: "name2",
-    mileage: "mileage2",
+    name: "HONDA cITY 5 Seater Car",
+    mileage: "20k",
     thumbnailSrc:
-      "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
-    dailyPrice: 1002,
-    monthlyPrice: 5002,
-    gearType: "gearType2",
-    gas: "gas2",
+      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+    dailyPrice: 50,
+    monthlyPrice: 1500,
+    gearType: "Auto",
+    gas: "Petrol",
   };
+  console.log({ ...testCar });
+
+  const cars = [
+    <Car {...testCar} />,
+    <Car {...testCar2} />,
+    <Car {...testCar} />,
+    <Car {...testCar2} />,
+    <Car {...testCar} />,
+    <Car {...testCar2} />,
+  ];
+
+  const numberOfDots = isMobile ? cars.length : Math.ceil(cars.length / 3);
+
   return (
     <TopCarsContainer>
       <Title>Explore Our Top Deals</Title>
       <CarsContainer>
-        <Car {...testCar} />
-        <Car {...testCar} />
-        <Car {...testCar2} />
+        <Carousel
+          value={current}
+          onChange={setCurrent}
+          slides={cars}
+          plugins={[
+            "clickToChange",
+            {
+              resolve: slidesToShowPlugin,
+              options: {
+                numberOfSlides: 3,
+              },
+            },
+          ]}
+          breakpoints={{
+            640: {
+              plugins: [
+                {
+                  resolve: slidesToShowPlugin,
+                  options: {
+                    numberOfSlides: 1,
+                  },
+                },
+              ],
+            },
+            900: {
+              plugins: [
+                {
+                  resolve: slidesToShowPlugin,
+                  options: {
+                    numberOfSlides: 2,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+        <Dots value={current} onChange={setCurrent} number={numberOfDots} />
       </CarsContainer>
     </TopCarsContainer>
   );
 }
-
-export default TopCars;
