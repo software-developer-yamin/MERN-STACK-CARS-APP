cac6c81b6d27c2bf9dd1c35ea8da1b4138ffbe02
DeveloperYamin <developer.yamin@gmail.com>
2022-02-19 21:27:59 +0600

    Adding responsivness to booking steps for mobile

--- src/app/components/containers/HomePage/bookingSteps.tsx
+++ src/app/components/containers/HomePage/bookingSteps.tsx
@@ -1,27 +1,34 @@
-import { faCalendarAlt, faCarSide, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
-import styled from 'styled-components';
-import tw from "twin.macro"
+import {
+  faCalendarAlt,
+  faCarSide,
+  faMapMarkedAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 const Container = styled.div`
-     ${tw`
-          w-full
-          flex
-          flex-col
-          items-center
-          py-3
-          lg:py-6
-     `}
-`
+  ${tw`
+    w-full
+    flex
+    flex-col
+    items-center
+    pt-3
+    pb-3
+    lg:pt-6
+    lg:pb-6
+  `};
+`;
+
 const Title = styled.h2`
   ${tw`
-    text-xl
-    lg:text-4xl
+    text-3xl
+    lg:text-5xl
     text-black
     font-extrabold
-  `}
-`
+  `};
+`;
+
 const StepsContainer = styled.div`
   ${tw`
     flex
@@ -29,56 +36,62 @@ const StepsContainer = styled.div`
     flex-wrap
     mt-7
     lg:mt-16
-  `}
-`
+  `};
+`;
+
 const StepContainer = styled.div`
   ${tw`
     flex
     flex-col
-    w-16
     md:w-96
     items-center
     transition-colors
     hover:text-red-500
-  `}
-`
+    m-3
+  `};
+`;
+
 const Step = styled.div`
+  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
   ${tw`
     flex
     rounded-lg
     items-center
     justify-center
     p-6
-  `}
-`
+  `};
+`;
+
 const StepTitle = styled.h4`
   ${tw`
     text-black
     text-lg
     font-semibold
     mt-4
-  `}
-`
+  `};
+`;
+
 const StepDescription = styled.p`
   ${tw`
-    text-xs
     w-10/12
+    text-xs
     md:text-sm
     text-center
     text-gray-600
-  `}
-`
+  `};
+`;
+
 const StepIcon = styled.span`
   ${tw`
     text-red-500
     text-3xl
-  `}
-`
+  `};
+`;
 
-
-function BookingSteps() {
+export default function BookingSteps() {
   return (
     <Container>
+      <Title>Our Working Steps</Title>
       <StepsContainer>
         <StepContainer>
           <Step>
@@ -117,5 +130,3 @@ function BookingSteps() {
     </Container>
   );
 }
-
-export default BookingSteps;
